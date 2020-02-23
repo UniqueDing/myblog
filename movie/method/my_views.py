9925deee580276efaafefda7ce2c2f2a8b46e7e5
uniqueding <1224472370@qq.com>
2020-02-23 15:12:42 +0800

    modity movie behavior

--- movie/method/my_views.py
+++ movie/method/my_views.py
@@ -8,29 +8,47 @@ import math
 
 def movie_view(request):
     lang = language(request)
+    changed = request.GET.get('changed', None)
     try:
         movie_url = models.Movie.objects.values('url').get(id=1)
         movie_status = models.Movie.objects.values('status').get(id=1)
         movie_rate = models.Movie.objects.values('rate').get(id=1)
+        movie_changed = models.Movie.objects.values('changed').get(id=1)
     except models.Movie.DoesNotExist:
         raise Http404("does not exist")
-    context = {'lang': lang, 'movie_url' : movie_url['url'], 'movie_status' : movie_status['status'], 'movie_rate' : movie_rate['rate']}
+    if str(movie_changed['changed']) == changed:
+        is_admin = 'display'
+    else:
+        is_admin = 'none'
+    print(str(movie_changed['changed']) + ' ' + str(changed))
+    context = {
+                'lang': lang,
+                'movie_url' : movie_url['url'],
+                'movie_status' : movie_status['status'],
+                'movie_rate' : movie_rate['rate'],
+                'movie_changed' : is_admin,
+    }
     return context
 
 
 def movieset_view(request):
-    models.Movie.objects.filter(id='1').update(url=request.GET.get('movie_url', None))
-    models.Movie.objects.filter(id='1').update(status=request.GET.get('movie_status', None))
-    models.Movie.objects.filter(id='1').update(rate=request.GET.get('movie_rate', None))
-    models.Movie.objects.filter(id='1').update(changed=request.GET.get('movie_changed', None))
-    return 0
+    movie_changed = models.Movie.objects.values('changed').get(id=1)
+    if str(movie_changed['changed']) == request.GET.get('movie_changed', None):
+        models.Movie.objects.filter(id='1').update(url=request.GET.get('movie_url', None))
+        models.Movie.objects.filter(id='1').update(status=request.GET.get('movie_status', None))
+        models.Movie.objects.filter(id='1').update(rate=request.GET.get('movie_rate', None))
+        return 'true'
+    return 'false'
 
 
 def movieget_view(request):
     movie_url = models.Movie.objects.values('url').get(id=1)
     movie_status = models.Movie.objects.values('status').get(id=1)
     movie_rate = models.Movie.objects.values('rate').get(id=1)
-    movie_changed = models.Movie.objects.values('changed').get(id=1)
-    context = {'movie_url' : movie_url['url'], 'movie_status' : movie_status['status'], 'movie_rate' : movie_rate['rate'], 'movie_changed' : movie_changed['changed']}
+    context = {
+                'movie_url' : movie_url['url'],
+                'movie_status' : movie_status['status'],
+                'movie_rate' : movie_rate['rate']
+    }
     text = json.dumps(context)
     return text
